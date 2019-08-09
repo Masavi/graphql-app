0452ee7c34f16c15932ae50249b13914e0ad503b
maui <maui@devf.mx>
2019-08-08 20:45:36 -0500

    agregar cliente de apollo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import client from './Graphql';
 import Navbarcito from './components/common/Navbarcito';
 import routes from './Routes';
 
 function App() {
   return (
-    <Router>
-      <Navbarcito />
-      <div className="container mt-4">
-        <Switch>
-            { routes }
-        </Switch>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbarcito />
+        <div className="container mt-4">
+          <Switch>
+              { routes }
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
