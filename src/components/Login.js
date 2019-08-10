42656189a808285f2a3eae8af7c3c726bc966dff
maui <maui@devf.mx>
2019-08-09 19:53:40 -0500

    implementar login mutation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Input from './common/Input';
 import { Container, Row, Col, Button } from 'reactstrap';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const LOGIN = gql`
+    mutation LOGIN($email: String!, $password: String!){
+        login(email: $email, password: $password){
+            token
+        }
+    }
+`
 
 class Login extends Component {
     state = { 
-        first_name: '',
-        last_name: '',
-        birth_date: '',
-        gender: '',
         email: '',
         password: '',
-        profile_img: '',
     }
 
     handleInput = (e) => {
@@ -20,56 +25,69 @@ class Login extends Component {
         });
     }
 
-    hanldeForm = (e) => {
+    handleForm = (e, login) => {
         e.preventDefault();
         console.log('Enviando formulario...');
-        console.log(this.state);
+        login({variables: { 
+            ...this.state
+        }});
     }
 
     render() { 
-        return ( 
-            <React.Fragment>
-                <h1>Login</h1>
-                <form onSubmit={e => this.hanldeForm(e)}>
-                    <Container 
-                        style={{
-                            padding: '1em',
-                            borderRadius: '0.5em',
-                            backgroundColor: '#bdbbbb',
-                        }}
-                        className='mt-4'>
-                        <Row>
-                            <Col>
-                                <Input 
-                                id='email'
-                                name='Email'
-                                type='email' 
-                                value={this.state.email}
-                                setInput={this.handleInput}
-                                required={true} />
-                            </Col>
-                            <Col>
-                                <Input 
-                                id='password'
-                                name='Contraseña'
-                                type='password' 
-                                value={this.state.password}
-                                setInput={this.handleInput}
-                                required={true} />
-                            </Col>
-                        </Row>
-                    </Container>
-                    <div className='d-flex flex-column mt-2'>
-                        <Button
-                            type='submit'
-                            color='primary' 
-                            className='mt-2'>
-                            Enviar
-                        </Button>
-                    </div>
-                </form>
-            </React.Fragment>
-        );
+        return (
+            <Mutation mutation={LOGIN}>
+                {
+                    (login, {data, error, loading}) => {
+                        if (loading) console.log(loading);
+                        if (data) console.log(data);
+                        if (error) console.log(error)
+                        return ( 
+                            <React.Fragment>
+                                <h1>Login</h1>
+                                <form onSubmit={e => this.handleForm(e, login)}>
+                                    <Container 
+                                        style={{
+                                            padding: '1em',
+                                            borderRadius: '0.5em',
+                                            backgroundColor: '#bdbbbb',
+                                        }}
+                                        className='mt-4'>
+                                        <Row>
+                                            <Col>
+                                                <Input 
+                                                id='email'
+                                                name='Email'
+                                                type='email' 
+                                                value={this.state.email}
+                                                setInput={this.handleInput}
+                                                required={true} />
+                                            </Col>
+                                            <Col>
+                                                <Input 
+                                                id='password'
+                                                name='Contraseña'
+                                                type='password' 
+                                                value={this.state.password}
+                                                setInput={this.handleInput}
+                                                required={true} />
+                                            </Col>
+                                        </Row>
+                                    </Container>
+                                    <div className='d-flex flex-column mt-2'>
+                                        <Button
+                                            type='submit'
+                                            color='primary' 
+                                            className='mt-2'>
+                                            Enviar
+                                        </Button>
+                                    </div>
+                                </form>
+                            </React.Fragment>
+                        );
+                    } 
+                }
+            </Mutation>
+        )
     }
 }
  
