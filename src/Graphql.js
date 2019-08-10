e782016dca87c01658452cbcd5d8ac62b70fefea
maui <maui@devf.mx>
2019-08-09 20:35:28 -0500

    get mawiToken

--- src/Graphql.js
+++ src/Graphql.js
@@ -10,7 +10,7 @@ import { createUploadLink } from 'apollo-upload-client';
 })
 
  const authLink = setContext((_,{headers})=>{
-    const token = localStorage.getItem('appToken');
+    const token = localStorage.getItem('mawiToken');
     return {
         headers: {
             ...headers,
