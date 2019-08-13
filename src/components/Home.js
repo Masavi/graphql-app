ad2eb1beeda10af937507ac4e22857f52ef7a0dc
maui <maui@devf.mx>
2019-08-12 20:51:19 -0500

    get users query in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const GET_USERS = gql`
+  {
+    getUsers {
+        _id 
+        email
+        first_name
+        last_name
+    }
+  }
+`
 
 export class Home extends Component {
-    render() {
+
+    renderUsers = (users) => {
+        return users.map( user => {
+            return <div 
+                    style={{
+                        border: '2px solid black',
+                        padding: '10px',
+                        marginBottom: '6px',
+                    }}
+                    key={user._id}>
+                        <h3> { `${user.first_name}  ${user.last_name}` }</h3>
+                        <h4> <i> { `${user.email}` } </i> </h4>
+                   </div>
+        })
+    }
+
+    render() {       
         return (
-            <React.Fragment>
-                <h2>Soy el componente HOME</h2>
-            </React.Fragment>
+            <Query query={GET_USERS}>
+                {({ loading, error, data }) => {
+                    if(loading) return <h1>Cargando...</h1>
+                    if(error) return <h1>Hubo un error!</h1>
+                    if (data) return this.renderUsers(data.getUsers);
+                }}
+            </Query>
         )
     }
 }
